b8cabb823c5beec0d71ef9b28f0ad45468aba5bb
owil <lacheinne@gmail.com>
2015-12-07 13:53:03 -0500

    Removed ScoreAdapter
    Fixed some issues.

--- app/src/main/java/com/pressthatbutton/thirtysecondsmash/CountdownFragment.java
+++ app/src/main/java/com/pressthatbutton/thirtysecondsmash/CountdownFragment.java
@@ -28,6 +28,7 @@ public class CountdownFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     /**
