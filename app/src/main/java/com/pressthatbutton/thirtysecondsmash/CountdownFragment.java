1aafcda1edd43d36b65986896b2a21737f964788
owil <lacheinne@gmail.com>
2015-12-07 12:10:38 -0500

    Modified Countdown Fragment
    Minor changes to TextView and added interface.

--- app/src/main/java/com/pressthatbutton/thirtysecondsmash/CountdownFragment.java
+++ app/src/main/java/com/pressthatbutton/thirtysecondsmash/CountdownFragment.java
@@ -1,6 +1,7 @@
 package com.pressthatbutton.thirtysecondsmash;
 
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -26,5 +27,20 @@ public class CountdownFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
     }
 }
