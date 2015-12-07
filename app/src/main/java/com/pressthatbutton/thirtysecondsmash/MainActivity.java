f334f8b76dbe8665fba7633a82ba3f1f52540f61
Jerimy Tate <jerimy.tate@ge.com>
2015-12-07 17:27:56 -0500

    Attached Game Activity to button Press

--- app/src/main/java/com/pressthatbutton/thirtysecondsmash/MainActivity.java
+++ app/src/main/java/com/pressthatbutton/thirtysecondsmash/MainActivity.java
@@ -2,13 +2,10 @@ package com.pressthatbutton.thirtysecondsmash;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +23,11 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, GameActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
