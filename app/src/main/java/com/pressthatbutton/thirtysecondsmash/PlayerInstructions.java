1fe015e386fd8f855057aac5ebeec52414aa9237
owil <lacheinne@gmail.com>
2015-12-10 11:29:37 -0500

    Modified PlayerInstructions Java to load text
    Should have different colors in text. Not tested.

--- app/src/main/java/com/pressthatbutton/thirtysecondsmash/PlayerInstructions.java
+++ app/src/main/java/com/pressthatbutton/thirtysecondsmash/PlayerInstructions.java
@@ -3,6 +3,7 @@ package com.pressthatbutton.thirtysecondsmash;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,8 +19,13 @@ public class PlayerInstructions extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player_instructions);
 
+        String tempText ="When the button is <font color=#9bbb59>GREEN</font>, each press is +1 to score.";
         txtInstruction1 = (TextView)findViewById(R.id.txt_instructions_1);
+        txtInstruction1.setText(Html.fromHtml(tempText));
+
+        tempText = "When the button is <font color=#c0504d>RED</font>, each press is -1 to score.";
         txtInstruction2 = (TextView)findViewById(R.id.txt_instructions_2);
+        txtInstruction2.setText(Html.fromHtml(tempText));
 
         btnHelpToMain = (Button)findViewById(R.id.btnHelpToMain);
         btnHelpToMain.setOnClickListener(new View.OnClickListener() {
