b8205a9aac260d30ab6fcbf232fcf06837ba150c
Jerimy Tate <jerimy.tate@gmail.com>
2015-12-10 21:39:17 -0500

    added Play Again Button

--- app/src/main/java/com/pressthatbutton/thirtysecondsmash/PostGameActivity.java
+++ app/src/main/java/com/pressthatbutton/thirtysecondsmash/PostGameActivity.java
@@ -1,13 +1,34 @@
 package com.pressthatbutton.thirtysecondsmash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostGameActivity extends AppCompatActivity {
+
+    public Button btnPlayAgain;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_game);
+
+        btnPlayAgain = (Button)findViewById(R.id.btnplayAgain);
+        btnPlayAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {StartGame(v);}
+        });
+
     }
+
+        protected void StartGame(View view)
+        {
+            Intent intent = new Intent(PostGameActivity.this, GameActivity.class);
+            startActivity(intent);
+        }
+
 }
